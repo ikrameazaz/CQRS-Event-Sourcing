6f42f2ee0e936ae78a7f4a3d54699a9a4e427c38
ikrame <ikrameazaz@gmail.com>
2025-12-28 23:55:28 +0100

    commands

--- src/main/java/org/sid/demoescqrsaxon/commands/controllers/AccountCommandController.java
+++ src/main/java/org/sid/demoescqrsaxon/commands/controllers/AccountCommandController.java
@@ -1,13 +1,31 @@
 package org.sid.demoescqrsaxon.commands.controllers;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.apache.commons.lang3.text.translate.UnicodeUnpairedSurrogateRemover;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.sid.demoescqrsaxon.commands.commands.AddAccountCommand;
+import org.sid.demoescqrsaxon.commands.dto.AddNewAcountRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/commands/accounts")
 public class AccountCommandController {
 
-    public string AddNewAccount(AddNewAcountRequestDto request) {
+    public CommandGateway commandGateway;
 
+    public AccountCommandController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+@PostMapping("/add")
+    public String AddNewAccount(@RequestBody AddNewAcountRequestDto request) {
+        return commandGateway.send(new AddAccountCommand(
+                UUID.randomUUID().toString(),
+                request.initialBalance(),
+                request.currency()
+        ));
     }
 }
